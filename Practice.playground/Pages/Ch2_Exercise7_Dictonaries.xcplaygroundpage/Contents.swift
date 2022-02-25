e1c0ef3e3acbd6bb1ee5b6fc6d8e2848fd28cead
Shubham Vyas <svyas878@gmail.com>
2022-02-25 18:25:12 +0530

    Exercise 1 of Chapter 2 iOS done

--- Practice.playground/Pages/Ch2_Exercise7_Dictonaries.xcplaygroundpage/Contents.swift
+++ Practice.playground/Pages/Ch2_Exercise7_Dictonaries.xcplaygroundpage/Contents.swift
@@ -42,6 +42,61 @@ var people: [[String:String]] = [
 ]
 
 
+var students: [[String:Any]] = [
+
+[
+
+"firstName": "Calvin",
+
+"lastName": "Newton",
+
+"score": 13
+
+],
+
+[
+
+"firstName": "Garry",
+
+"lastName": "Mckenzie",
+
+"score": 12
+
+],
+
+[
+
+"firstName": "Leah",
+
+"lastName": "Rivera",
+
+"score": 10
+
+],
+
+[
+
+"firstName": "Sonja",
+
+"lastName": "Moreno",
+
+"score": 3
+
+],
+
+[
+
+"firstName": "Noel",
+
+"lastName": "Bowen",
+
+"score": 16
+
+]
+
+]
+
+
 //Chapter 2 - Excersice 7 - A - You are given a dictionary code of type [String:String] which has values for all lowercase letters. The codedictionary represents a way to encode a message. For example if code[a] = z and code[b] = x the encoded version if ababa will be zxzxz. You are also given a encodedMessage which contains only lowercase letters and spaces. Use the codedictionary to decode the message and print it. Write a Swift program to decode the message and print final output string
 var encodedMessage = "uijt nfttbhf jt ibse up sf4be"
 decodeMessage(encodedMessage: encodedMessage)
@@ -75,6 +130,93 @@ func getFirstName(people: [[String:String]]) {
         }
     }
     print(firstNames)
+}
+
+fullName(people: people)
+
+func fullName(people: [[String:String]]) {
+    
+    var names: [String] = []
+    var fullName = ""
+    for i in people {
+        
+        if let val = i.first(where: { $0.key == "firstName"})?.value {
+            fullName = val;
+        }
+        if let val = i.first(where: { $0.key == "lastName"})?.value {
+            fullName += " " + val;
+        }
+        names.append(fullName)
+    }
+    print(names)
+}
+
+getWeakestStudent(people:students)
+
+func getWeakestStudent(people: [[String:Any]]) {
+    
+    var score = 200
+    var fullName = ""
+    for i in people {
+        
+        if let val = i.first(where: { $0.key == "score" && ($0.value as! Int) < score})?.value {
+            
+            if let val = i.first(where: { $0.key == "firstName"})?.value {
+                fullName = val as! String
+            }
+            if let val = i.first(where: { $0.key == "lastName"})?.value {
+                fullName += " " + (val as! String)
+            }
+            score = val as! Int;
+        }
+        
+    }
+    print(fullName)
+}
+
+
+getStudentList(people: students)
+func getStudentList(people: [[String:Any]]) {
+    
+    var fullName = ""
+    var id = 1
+    for i in people {
+        if let val = i.first(where: { $0.key == "firstName"})?.value {
+            fullName = String(id) + " " + (val as! String)
+        }
+        if let val = i.first(where: { $0.key == "lastName"})?.value {
+            fullName += " " + (val as! String)
+        }
+        if let val = i.first(where: { $0.key == "score"})?.value {
+            fullName += " " + String((val as! Int))
+        }
+        id += 1
+        print(fullName)
+    }
+}
+
+
+getFreq()
+
+func getFreq() {
+    
+    var numbers = [1, 2, 3, 2, 3, 5, 2, 1, 3, 4, 2, 2, 2]
+    var dict: [Int:Int] = [:]
+    
+    
+    for i in 0...numbers.count-1 {
+        
+        if(dict[numbers[i]] == nil) {
+            dict[numbers[i]] = 0
+        }
+        dict[numbers[i]]! += 1;
+    }
+    
+    var keys = dict.keys.sorted()
+    
+    for i in keys {
+        print("\(i)  \(dict[i]!)")
+    }
 }
 
 
